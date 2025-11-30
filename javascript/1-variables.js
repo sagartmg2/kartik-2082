221485a022723a58fd4ba7f6d0988b9b2a767235
dev.sagartmg <dev.sagartmg@gmail.com>
2025-11-30 09:32:46 +0545

    1-variables

---
+++ javascript/1-variables.js
@@ -0,0 +1,42 @@
+// line comment
+/* block comment alt+ shift+ a */
+
+/*  
+    variables:
+    simply a keyword which can store some values.
+    main purpose: REUSE
+
+    analogy: think it like a box.
+
+    variable name should be meaningful
+
+
+    var vs let vs  const
+
+    DATA-TYPES:
+    text: string
+
+    number
+        112 : integer (int)
+        112.23 : float 
+
+    true/false : boolean
+
+*/
+
+
+address = "putalisadak";
+address = "bagbazar, putalisadak";
+address = "dillizbar putalisadak";
+adress = `sundhara`;
+receiver = "sagar tamang";
+phone = 9840423455;
+is_premium_user = true; // snake_case
+isPremiumUser = true //camelCase
+
+abc = "john doe";
+console.log(abc);
+
+console.log("address: ", address);
+console.log("phone: ", phone);
+console.log("premiumuser", isPremiumUser);
