abba097250b7988b23fa651bffd0a4ba1057f915
dev.sagartmg <dev.sagartmg@gmail.com>
2025-12-30 09:55:11 +0545

    todos crud in react

--- my-base-react-project/src/Todo.jsx
+++ my-base-react-project/src/Todo.jsx
@@ -1,31 +1,97 @@
 import { useState } from "react";
 
 export const Todo = () => {
-  const [todos, setTodos] = useState(["toto-1", "todo-2"]);
+  const [todos, setTodos] = useState(["one"]);
+  const [showModal, setShowModal] = useState(false);
 
   const addNewItem = (e) => {
     e.preventDefault();
-    console.log(e.target.title.value);
+    console.log();
+    let newTodo = e.target.title.value;
+    let newTodoStatus = e.target.status.checked;
+    console.log(newTodoStatus);
+    console.log({ newTodo });
 
-    setTodos([e.target.title.value])
+    // let newTodos = todos  // ["one" ] // THIS will point to the actual reference of orginal todos variable
+    // newTodos.push(newTodo)
+    // setTodos(newTodos);
+
+    let newTodos = [...todos]; // [one]
+    newTodos.push(newTodo); // [one, two]
+    setTodos(newTodos);
   };
 
+  const editItem = () => {};
   return (
     <>
-      <form className="flex gap-4" onSubmit={addNewItem}>
-        <input type="checkbox" className="text-4xl h-12 w-12" name="title" />
+      <form className="flex items-center gap-4" onSubmit={addNewItem}>
+        <input type="checkbox" className="text-4xl h-8 w-8" name="status" />
         <input className="border py-4 px-8" placeholder="title" name="title" />
         <button className="border px-4 py-2 rounded hover:bg-red-600 hover:text-white hover:border-red-600 cursor-pointer">
           submit
         </button>
       </form>
       <br />
+      <div>{JSON.stringify(todos)}</div>
       <br />
-      <ul className="list-decimal">
-        {todos.map((el) => {
-          return <li>{el}</li>;
-        })}
-      </ul>
+      <table>
+        <thead>
+          <tr>
+            <th>title</th>
+            <th>status</th>
+            <th>action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {todos.map((el) => {
+            return (
+              <tr>
+                <td>{el}</td>
+                <td>yes</td>
+                <td>
+                  <button className="border px-4 py-2 rounded hover:bg-red-600 hover:text-white hover:border-red-600 cursor-pointer">
+                    edit
+                  </button>
+                  &nbsp;
+                  <button className="border px-4 py-2 rounded hover:bg-red-600 hover:text-white hover:border-red-600 cursor-pointer">
+                    delete
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+
+      {showModal && (
+        <div className="  bg-[rgba(0,0,0,0.7)] fixed left-0 right-0 top-0 bottom-0 flex justify-center items-center ">
+          <form
+            className="flex flex-col items-start gap-4 bg-white p-8"
+            onSubmit={editItem}
+          >
+            <p className="text-2xl">Edit Todo</p>
+            <div>
+              <label htmlFor="status">status</label>{" "}
+              <input
+                type="checkbox"
+                id="status"
+                className="text-4xl h-8 w-8"
+                name="status"
+              />
+            </div>
+            <div>
+              <input
+                className="border py-4 px-8"
+                placeholder="title"
+                name="title"
+              />
+            </div>
+            <button className="border px-4 py-2 rounded hover:bg-red-600 hover:text-white hover:border-red-600 cursor-pointer">
+              submit
+            </button>
+          </form>
+        </div>
+      )}
     </>
   );
 };
