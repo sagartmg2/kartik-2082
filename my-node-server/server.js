c08c6f2aa3eefb10172b51718113e87318e5e187
dev.sagartmg <dev.sagartmg@gmail.com>
2026-01-16 09:13:03 +0545

    middlewares

--- my-node-server/server.js
+++ my-node-server/server.js
@@ -1,10 +1,39 @@
 const express = require("express");
 // import express from "express"
+const cors = require("cors");
 
 const app = express();
 const PORT = 3000;
 
-app.use(express.json()); // middleware
+/*  
+
+function cors(){
+  return <function>
+}
+
+*/
+
+/* middlewares
+   simple a function which has access to req and response and has ability to alter them 
+   and also has access to next middleware in line
+
+*/
+
+app.use(cors());
+app.use(express.json()); // global middleware
+
+const checkAuthentication = (req, res, next) => {
+  console.log("checkAuthentication");
+  //   check token in req.headers and validate it.
+  let loggedIn = false;
+  if (loggedIn) {
+    next();
+  } else {
+    return res.status(401).send({ msg: "unauthenticated" });
+  }
+};
+
+// app.use(checkAuthentication); // global middeware
 
 /* 
     CRUD operations
@@ -67,15 +96,29 @@ let products = [
 ];
 
 app.get("/api/products", (req, res) => {
+  // let products = DB::find("products")
   res.send(products);
 });
 
 app.post("/api/products", (req, res) => {
-  console.log(req.body);
+  if (!req.body.title || !req.body.price) {
+    return res.status(400).send("title or price missing");
+  }
+
+  console.log("req.body", req.body);
   products.push(req.body);
   res.send("product created");
 });
 
+/* route level middelware */
+app.get("/api/dashboard", checkAuthentication, (req, res) => {
+  res.send({
+    total: 101,
+    message: "sensitive infomation",
+  });
+});
+
+// start the server
 app.listen(PORT, () => {
   console.log(`server started at http://localhost:${PORT}`);
 });
